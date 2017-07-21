e1dd595cc46b666bbc68a493d1681f9843f1ff0f
crizzo71 <crizzo@redhat.com>
2017-07-21 15:37:06 -0400

    update dates and US

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .utils import *
+from datetime import *
 def home(request):
     return render(request, 'blog/home.html')
 def story_list(request):
     rally = initRally()
-    query_criteria = 'State != ""'
-    #'BusOpsKanban != "Backlog"
-    response = rally.get('Feature',fetch = True) #query=query_criteria)
+    #query_criteria = 'State != ""'
+    query_criteria='BusOpsKanban != "Backlog" AND CreationDate > "2014-12-31T00:00:00.0Z"'
+    response = rally.get('UserStory',fetch = True, query=query_criteria)
     #print (response)
 
     story_list = []
@@ -14,16 +15,17 @@ def story_list(request):
         for story in response:
             #print (story.details())
             a_story={}
-            a_story['State'] = story.State.Name if story.State else "Backlog"
+            #a_story['State'] = story.State.Name #if story.State else "Backlog"
+            a_story['State']=story.BusOpsKanban
             a_story['id'] = story.FormattedID
             a_story['name'] = story.Name
-            a_story['Opened']=story.CreationDate
+            a_story['Opened']=datetime.strptime(story.CreationDate, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%d-%b-%Y')
             a_story['Requester']= story.Owner.Name if story.Owner else "unassigned"
            #a_story['Kanban'] = story.c_BizOpsKanbanState
             story_list.append(a_story)
     else:
         story_list = response.errors
-    print(story_list)
+    print(story_list[0])
     return render(request, 'blog/story_list.html', {'stories': story_list})
 def graph(request):
     rally = initRally()
