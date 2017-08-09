305bbaafb03a9489e1a7f82fdc12e40b373d87e7
crizzo71 <crizzo@redhat.com>
2017-08-09 14:41:34 -0400

    Augustupdate

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,10 @@ def home(request):
 def story_list(request):
     rally = initRally()
     #query_criteria = 'State != ""'
-    query_criteria='BusOpsKanban != "Backlog" AND CreationDate > "2014-12-31T00:00:00.0Z"'
-    response = rally.get('UserStory',fetch = True, query=query_criteria)
-    #print (response)
+    query_criteria='BusOpsKanban != "Verified"'#AND CreationDate > "2014-12-31T00:00:00.0Z"
+    response = rally.get('HierarchicalRequirement', fetch = True, query=query_criteria)
+    response_defect = rally.get('Defect', fetch = True, query=query_criteria)
+    #print (response_defect)
 
     story_list = []
     if not response.errors:
@@ -16,16 +17,28 @@ def story_list(request):
             #print (story.details())
             a_story={}
             #a_story['State'] = story.State.Name #if story.State else "Backlog"
-            a_story['State']=story.BusOpsKanban
+            a_story['State']=story.BusOpsKanban if story.BusOpsKanban else "unassigned"
+            #a_story['Status']=Story.Status if story.Status else "unassigned"
             a_story['id'] = story.FormattedID
             a_story['name'] = story.Name
-            a_story['Opened']=datetime.strptime(story.CreationDate, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%d-%b-%Y')
+            a_story['Opened']=(datetime.strptime(story.CreationDate, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%d-%b'))
             a_story['Requester']= story.Owner.Name if story.Owner else "unassigned"
+            a_story['Blocked']= story.Blocked
            #a_story['Kanban'] = story.c_BizOpsKanbanState
             story_list.append(a_story)
+    if not response_defect.errors:
+        for story in response_defect:
+            a_story['State']=story.BusOpsKanban if story.BusOpsKanban else "unassigned"
+            #a_story['Status']=Story.Status if story.Status else "unassigned"
+            a_story['id'] = story.FormattedID
+            a_story['name'] = story.Name
+            a_story['Opened']=(datetime.strptime(story.CreationDate, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%d-%b'))
+            a_story['Requester']= story.Owner.Name if story.Owner else "unassigned"
+            a_story['Blocked']= story.Blocked
+           #a_story['Kanban'] = story.c_BizOpsKanbanState
     else:
         story_list = response.errors
-    print(story_list[0])
+    #print(story_list[0])
     return render(request, 'blog/story_list.html', {'stories': story_list})
 def graph(request):
     rally = initRally()
