549651ad1376e98bca2336a891407481eb6cc012
crizzo71 <crizzo@redhat.com>
2017-09-11 14:24:59 -0400

    September version

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,13 @@ def home(request):
     return render(request, 'blog/home.html')
 def story_list(request):
     rally = initRally()
-    #query_criteria='c_BusOpsKanban != "Verified"' #CreationDate > "2014-12-31T00:00:00.0Z"
-    #query_criteria='FormattedID = "US128736"'
-    query_criteria = 'c_BusOpsKanban != ""'
-    response = rally.get('HierarchicalRequirement', fetch = True, query=query_criteria)
+    query_criteria='c_BusOpsKanban != "Verified"' #CreationDate > "2014-12-31T00:00:00.0Z"
+    #query_criteria='FormattedID = "US75327"'
+    #query_criteria = 'c_BusOpsKanban != ""'
+    response = rally.get('UserStory', fetch = True, query=query_criteria)
     response_defect = rally.get('Defect', fetch = True, query=query_criteria)
     #print (response_custom)
-
+    #HierarchicalRequirement
     story_list = []
     if not response.errors:
         for story in response:
@@ -26,9 +26,11 @@ def story_list(request):
             a_story['Opened']=(datetime.strptime(story.CreationDate, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%d-%b'))
             a_story['Requester']= story.Owner.Name if story.Owner else "unassigned"
             a_story['Blocked']= story.Blocked
-            a_story['Service']=story.ServiceNowID.__dict__
-            #varx=story.ServiceNowID.__dict__
-            #print(type(varx))
+            a_story['Service']= getattr(story.ServiceNowID,'LinkID','N/A')
+            #print (story.ServiceNowID.__dict__)
+            #a_story['Service']=story.ServiceNowID.__dict__
+            #varx=story.ServiceNowID
+            #print(getattr(varx,'LinkID','N/A'))
             #for y in varx:
                 #print (y.Name)
             #attr_name =""
@@ -41,14 +43,14 @@ def story_list(request):
             story_list.append(a_story)
     if not response_defect.errors:
         for story in response_defect:
+            a_story={}
             a_story['State']=story.BusOpsKanban if story.BusOpsKanban else "unassigned"
-            #a_story['Status']=Story.Status if story.Status else "unassigned"
             a_story['id'] = story.FormattedID
             a_story['name'] = story.Name
             a_story['Opened']=(datetime.strptime(story.CreationDate, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%d-%b'))
             a_story['Requester']= story.Owner.Name if story.Owner else "unassigned"
             a_story['Blocked']= story.Blocked
-           #a_story['Kanban'] = story.c_BizOpsKanbanState
+            a_story['Service']= getattr(story.ServiceNowID,'LinkID','N/A')
             story_list.append(a_story)
     else:
         story_list = response.errors
